1652743536aa2426806d7cc65ea54c56e9c4a105
Parallax <narakagaara@gmail.com>
2024-03-11 00:23:09 -0300

    router service added

---
+++ frontend/src/app/game/service/router/router.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RouterService {
+
+  constructor(private router: Router) { }
+
+  redirectToLogin() {
+    this.router.navigate(['/login']).catch(error => {
+      console.error('Error navigating to /login:', error);
+    });
+  }
+
+  redirectToDoc(){
+    this.router.navigate(['/documentation']).catch(error => {
+      console.error('Error navigating to /documentation:', error);
+    });
+  }
+  redirectToMenu(){
+    this.router.navigate(['/game']).catch(error => {
+      console.error('Error navigating to /game:', error);
+    });
+  }
+
+  redirectToCode() {
+    window.location.href = 'https://github.com/Parallax73/Soundle';
+  }
+
+  redirectToRegister(){
+    this.router.navigate(['/register']).catch(error => {
+      console.error('Error navigating to /register:', error);
+    });
+  }
+}
