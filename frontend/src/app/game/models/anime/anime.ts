87a60631057ddd91496fa01fd91523006a2f6e9f
Parallax <narakagaara@gmail.com>
2024-04-24 22:25:50 -0300

    added anime class

---
+++ frontend/src/app/game/models/anime/anime.ts
@@ -0,0 +1,15 @@
+export class Anime {
+
+  audioLink: string;
+  name: string;
+  videoLink: string;
+  position: number;
+
+  constructor() {
+    this.audioLink = "";
+    this.name = "";
+    this.videoLink = "";
+    this.position = 0;
+  }
+
+}
