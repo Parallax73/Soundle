43ce40a1c9b95a80920fc39b6a48ab4ae365fbea
Parallax <narakagaara@gmail.com>
2024-05-06 23:54:22 -0300

    Added volumeChange

--- frontend/src/app/game/components/common/guess-card/guess-card.component.ts
+++ frontend/src/app/game/components/common/guess-card/guess-card.component.ts
@@ -1,55 +1,95 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MatProgressSpinner, ProgressSpinnerMode} from "@angular/material/progress-spinner";
 import {MatSlider, MatSliderThumb} from "@angular/material/slider";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {MatButtonToggle} from "@angular/material/button-toggle";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {ToolbarComponent} from "../toolbar/toolbar.component";
 
-@Component({
+
+  @Component({
   selector: 'app-guess-card',
   standalone: true,
-  imports: [
-    MatProgressSpinner,
-    MatSlider,
-    MatSliderThumb,
-    MatButton,
-    MatIcon,
-    NgIf
-  ],
+    imports: [
+      MatProgressSpinner,
+      MatSlider,
+      MatSliderThumb,
+      MatButton,
+      MatIcon,
+      NgIf,
+      MatButtonToggle,
+      MatFormField,
+      MatInput,
+      NgForOf,
+      NgOptimizedImage,
+      MatIconButton,
+      FormsModule,
+      ToolbarComponent
+    ],
   templateUrl: './guess-card.component.html',
   styleUrl: './guess-card.component.scss'
 })
-export class GuessCardComponent  {
-  mode: ProgressSpinnerMode = 'determinate';
-  playOrPause = true;
-  value: number = 0;
-  maxValue: number = 100;
+  export class GuessCardComponent implements OnInit{
+    @ViewChild('audioPlayer') audioPlayer!: ElementRef
+    mode: ProgressSpinnerMode = 'indeterminate';
+    playOrPause = true;
 
+    volumeValue: any = null;
+    isAudioPlaying: boolean = false;
+    timer: number = 15;
+    interval: any = null;
+    audioSource = "";
+    loading = true;
+    userInput = "";
 
+    volumeChange(){
+      this.audioPlayer.nativeElement.volume = this.volumeValue/100;
+    }
 
-  changeToPause() {
-    this.playOrPause = !this.playOrPause;
-  }
-
-  play(){
-
-  }
-
- startCount(targetTime:number){
-    const incrementPerMillisecond = this.maxValue / targetTime;
-    const intervalDuration = 1 / incrementPerMillisecond;
-
-    const interval = setInterval(() => {
-      this.value += incrementPerMillisecond * intervalDuration;
-      if (this.value >= this.maxValue) {
-        clearInterval(interval);
-        this.value = this.maxValue;
+    playAudio(): void {
+      if(!this.isAudioPlaying) {
+        this.audioPlayer.nativeElement.play();
+        this.isAudioPlaying = true;
+        this.timerCounter()
+      } else {
+        this.audioPlayer.nativeElement.pause();
+        this.audioPlayer.nativeElement.currentTime = 0;
+        this.isAudioPlaying = false;
+        this.timer = 15;
+        clearInterval(this.interval);
       }
-    }, intervalDuration);
+    }
 
-    setTimeout(() => {
-      clearInterval(interval);
-      this.value = this.maxValue;
-    }, targetTime);
+    ngOnInit() {
+      setTimeout(() => {
+        this.loading = false;
+      }, 3000);
+    }
+
+
+    timerCounter(){
+      this.interval = setInterval(() => {
+        if (this.timer>0) {
+          this.timer--;
+        } else {
+          clearInterval(this.interval);
+        }
+      }, 1000)
+    }
+
+    checkAnswer(){
+      console.log(this.userInput);
+    }
+
+    play() {
+      this.playAudio();
+      this.playOrPause = !this.playOrPause;
+    }
   }
-}
+
+
+
