981b06e0f655ecee17c61b49689d8b9a1afdd8db
Parallax <narakagaara@gmail.com>
2024-05-15 00:48:25 -0300

    Refactored ngOnInit, added ShuffleLoop and ShuffleSong methods

--- frontend/src/app/game/components/quiz/quiz.component.ts
+++ frontend/src/app/game/components/quiz/quiz.component.ts
@@ -1,15 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GameOptionsComponent} from "../common/game-options/game-options.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {GuessCardComponent} from "../common/guess-card/guess-card.component";
 import {ToolbarComponent} from "../common/toolbar/toolbar.component";
-import {MatCard, MatCardContent} from "@angular/material/card";
-import {ArtCardComponent} from "../common/art-card/art-card.component";
+import {MatProgressSpinner, ProgressSpinnerMode} from "@angular/material/progress-spinner";
 import {MatButtonToggle} from "@angular/material/button-toggle";
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatError, MatFormField} from "@angular/material/form-field";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
-import {NgForOf, NgIf} from "@angular/common";
-import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {TrackService} from "../../service/track/track.service";
+import {Track} from "../../models/track/track";
+import {Observable} from "rxjs";
+
 
 interface Category {
   value: string;
@@ -20,34 +23,46 @@ interface Category {
   selector: 'app-quiz',
   standalone: true,
   imports: [
+    GameOptionsComponent,
+    NgIf,
+    GuessCardComponent,
     ToolbarComponent,
-    MatCard,
-    MatCardContent,
-    ArtCardComponent,
+    MatProgressSpinner,
     MatButtonToggle,
+    FormsModule,
+    MatError,
     MatFormField,
     MatOption,
     MatSelect,
     NgForOf,
-    NgIf,
-    FormsModule,
-    ReactiveFormsModule,
-    MatError,
-    MatProgressSpinner
+    ReactiveFormsModule
+
   ],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.scss'
 })
-export class QuizComponent  {
+export class QuizComponent implements OnInit{
+  source = "";
+  loading = true;
+  mode: ProgressSpinnerMode = 'indeterminate';
 
-
+  timer: number = 0;
+  quantity: number = 0;
+  category!: Category;
+  hasBeenSelect: boolean = false;
+  choice = "";
+  track!: Track
 
   animeControl = new FormControl('', Validators.required);
   categoryControl = new FormControl('', Validators.required);
   quantityControl = new FormControl('', Validators.required);
   timeControl = new FormControl('', Validators.required);
+
   formGroup: any;
   service: any;
+
+  isCardVisible = true;
+  isSpinnerVisible = false;
 
   constructor(private formBuilder: FormBuilder, service: TrackService) {
     this.service = service;
@@ -59,13 +74,6 @@ export class QuizComponent  {
     });
   }
 
-  isCardVisible = true;
-  isSpinnerVisible = false;
-  selectedTime: string = '';
-  selectedQuantity: string = '';
-  selectedChoice: string = '';
-  selectedCategory: Category | null = null;
-
   categories: Category[] = [
     { value: 'c-0', viewValue: 'Anime' },
     { value: 'c-1', viewValue: 'Top Streamed' },
@@ -76,17 +84,17 @@ export class QuizComponent  {
   ];
 
   times: Category[] = [
-    { value: 't-0', viewValue: '15 seconds' },
-    { value: 't-2', viewValue: '10 seconds' },
-    { value: 't-3', viewValue: '5 seconds' },
-    { value: 't-4', viewValue: '2 seconds' }
+    { value: '15', viewValue: '15 seconds' },
+    { value: '10', viewValue: '10 seconds' },
+    { value: '5', viewValue: '5 seconds' },
+    { value: '2', viewValue: '2 seconds' }
   ];
 
-  quantity: Category[] = [
-    { value: 'q-0', viewValue: '15' },
-    { value: 'q-2', viewValue: '10' },
-    { value: 'q-3', viewValue: '5' },
-    { value: 'q-4', viewValue: 'Infinite' }
+  quantityOptions: Category[] = [
+    { value: '15', viewValue: '15' },
+    { value: '10', viewValue: '10' },
+    { value: '5', viewValue: '5' },
+    { value: '20', viewValue: '20' }
   ];
 
   animeChoices: Category[] = [
@@ -94,15 +102,13 @@ export class QuizComponent  {
     { value: 'video', viewValue: 'Opening Video' }
   ];
 
-
-
   onShuffleClick() {
     if (this.formGroup.valid) {
       this.isCardVisible = false;
       this.isSpinnerVisible = true;
-      this.service.teste();
-
-
+      this.hasBeenSelect = true;
+      this.timer = this.formGroup.get("time").value;
+      this.shuffleSong()
     } else {
       for (const name in this.formGroup.controls) {
         if (this.formGroup.controls[name].invalid) {
@@ -114,13 +120,62 @@ export class QuizComponent  {
 
   onChange(categoryValue: string) {
     const selectedCategory = this.categories.find(category => category.value === categoryValue);
-    if (selectedCategory && selectedCategory.viewValue === 'Anime') {
-      this.animeControl.setValidators([Validators.required]);
-    } else {
-      this.animeControl.clearValidators();
+    if (selectedCategory) {
+      if (selectedCategory.viewValue === 'Anime') {
+        this.animeControl.setValidators([Validators.required]);
+      } else {
+        this.animeControl.clearValidators();
+      }
+      this.animeControl.updateValueAndValidity();
     }
-    this.animeControl.updateValueAndValidity();
   }
+
+  shuffleSong(){
+    this.choice = this.formGroup.get("category").value;
+    switch (this.choice){
+      case "c-0": // anime
+        this.shuffleLoop(this.service.drawAnime.bind(this.service));
+        break;
+      case "c-1": //top
+        this.shuffleLoop(this.service.drawTop.bind(this.service));
+        break;
+      case "c-2": // 2000s
+        this.shuffleLoop(this.service.draw2000().bind(this.service));
+        break;
+      case "c-3": // 90s
+        this.shuffleLoop(this.service.draw90s.bind(this.service));
+        break;
+      case "c-4": // 80s
+        this.shuffleLoop(this.service.draw80s.bind(this.service));
+        break;
+      case "c-5": // 70s
+        this.shuffleLoop(this.service.draw70s.bind(this.service));
+        break;
+    }
+  }
+
+  shuffleLoop(serviceCall: () => Observable<Track>){
+   /* this.quantity = this.formGroup.get("quantity").value;
+    while(this.quantity > 0){*/
+    serviceCall().subscribe(track => {
+      this.track = track;
+    });
+
+    console.log(this.track.audio , this.track.artist, this.track.album, this.track.title);
+
+     /* this.quantity--;
+    }*/
+  }
+
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+  }
+
+
 
 
 
