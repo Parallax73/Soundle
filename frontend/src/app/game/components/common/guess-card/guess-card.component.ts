59a5ff61bc3a3ea068b93a2fb38131fed6784476
Parallax <narakagaara@gmail.com>
2024-05-15 00:50:31 -0300

    Refactored TimerCounter and volumeChange methods

--- frontend/src/app/game/components/common/guess-card/guess-card.component.ts
+++ frontend/src/app/game/components/common/guess-card/guess-card.component.ts
@@ -1,14 +1,19 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {MatProgressSpinner, ProgressSpinnerMode} from "@angular/material/progress-spinner";
 import {MatSlider, MatSliderThumb} from "@angular/material/slider";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatIcon} from "@angular/material/icon";
-import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {MatButtonToggle} from "@angular/material/button-toggle";
 import {MatFormField} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {ToolbarComponent} from "../toolbar/toolbar.component";
+import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from "@angular/material/autocomplete";
+import {map, Observable, of, startWith} from "rxjs";
+import {AutocompleteService} from "../../../service/autocomplete/autocomplete.service";
+import {RouterOutlet} from "@angular/router";
+
 
 
   @Component({
@@ -28,66 +33,79 @@ import {ToolbarComponent} from "../toolbar/toolbar.component";
       NgOptimizedImage,
       MatIconButton,
       FormsModule,
-      ToolbarComponent
+      ToolbarComponent,
+      ReactiveFormsModule,
+      MatAutocompleteTrigger,
+      MatOption,
+      MatAutocomplete,
+      AsyncPipe,
+      RouterOutlet
     ],
   templateUrl: './guess-card.component.html',
   styleUrl: './guess-card.component.scss'
 })
   export class GuessCardComponent implements OnInit{
-    @ViewChild('audioPlayer') audioPlayer!: ElementRef
-    mode: ProgressSpinnerMode = 'indeterminate';
-    playOrPause = true;
 
-    volumeValue: any = null;
-    isAudioPlaying: boolean = false;
-    timer: number = 15;
-    interval: any = null;
-    audioSource = "";
-    loading = true;
+    @ViewChild('audioPlayer', { static: false }) audioPlayer!: ElementRef;
+    @Input('source') source!: string;
+    @Input('timer') timer!: number;
     userInput = "";
+    myControl = new FormControl();
+    volumeValue: number = 0;
+    options: string[] = [];
+    playOrPause = false;
+    filteredOptions: Observable<string[]> = of([]);
+    isAudioPlaying = false;
+    interval: any = null;
 
-    volumeChange(){
-      this.audioPlayer.nativeElement.volume = this.volumeValue/100;
-    }
-
-    playAudio(): void {
-      if(!this.isAudioPlaying) {
-        this.audioPlayer.nativeElement.play();
-        this.isAudioPlaying = true;
-        this.timerCounter()
-      } else {
-        this.audioPlayer.nativeElement.pause();
-        this.audioPlayer.nativeElement.currentTime = 0;
-        this.isAudioPlaying = false;
-        this.timer = 15;
-        clearInterval(this.interval);
-      }
+    constructor(private autocomplete: AutocompleteService) {
+      this.autocomplete.getNames().subscribe(names => this.options = names);
     }
 
     ngOnInit() {
-      setTimeout(() => {
-        this.loading = false;
-      }, 3000);
-    }
-
-
-    timerCounter(){
-      this.interval = setInterval(() => {
-        if (this.timer>0) {
-          this.timer--;
-        } else {
-          clearInterval(this.interval);
-        }
-      }, 1000)
+      if (this.audioPlayer && this.audioPlayer.nativeElement) {
+        console.log('audioPlayer:', this.audioPlayer);
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+          startWith(''),
+          map(value => this.autocomplete.filterNames(value))
+        );
+      }
     }
 
     checkAnswer(){
       console.log(this.userInput);
     }
 
-    play() {
-      this.playAudio();
-      this.playOrPause = !this.playOrPause;
+    playAudio(): void {
+      if (!this.isAudioPlaying) {
+        this.audioPlayer.nativeElement.play().catch((error: any) => console.log('Playback failed because: ', error));
+        this.isAudioPlaying = true;
+        this.timerCounter();
+      } else {
+        this.audioPlayer.nativeElement.pause();
+        this.isAudioPlaying = false;
+        clearInterval(this.interval); // Stop the timer
+      }
+      this.playOrPause = this.isAudioPlaying;
+    }
+
+    timerCounter(){
+      this.interval = setInterval(() => {
+        if (this.timer > 0) {
+          this.timer--;
+        } else {
+          this.audioPlayer.nativeElement.pause();
+          this.audioPlayer.nativeElement.currentTime = 0;
+          this.isAudioPlaying = false;
+          this.playOrPause = this.isAudioPlaying;
+          clearInterval(this.interval);
+        }
+      }, 1000)
+    }
+
+
+    volumeChange(volume:number){
+      this.audioPlayer.nativeElement.volume = volume/100;
     }
   }
 
